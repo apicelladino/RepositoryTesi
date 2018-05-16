8f45a75209d0bcbd65fc5f06655bd44c9af7c572
apicelladino <38455901+apicelladino@users.noreply.github.com>
2018-05-16 17:33:06 +0200

    Add files via upload

--- HAR_PROJECT/src/main/java/io/github/aqibsaeed/activityrecognition/MainActivity.java
+++ HAR_PROJECT/src/main/java/io/github/aqibsaeed/activityrecognition/MainActivity.java
@@ -18,7 +18,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
+    private final double gConst = 9.81;
     private final int N_SAMPLES = 50;
     private TextView breakprob;
     private TextView situpprob;
@@ -32,19 +32,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor mOrientationAngles;
     private Sensor mGyroscope;
     private Sensor mGravity;
+    private Sensor mGeomagnetic;
     //private float xGrav,yGrav,zGrav,xAcc,yAcc,zAcc,pitch,roll,yaw,xRot,yRot,zRot;
     private static List xGrav, yGrav, zGrav, xAcc, yAcc, zAcc, pitch, roll, yaw, xRot, yRot, zRot;
-    private static List input_signal;
+    private static List input_signal, input_collection;
+    private int breakcount, situpcount, burpeecount, squatcount;
+    private float[] mGrav;
+    private float[] mGeo;
+    private double p, r, y, xG, yG, zG, xA, yA, zA, xR, yR, zR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        breakprob = (TextView)findViewById(R.id.breakprob);
-        situpprob = (TextView)findViewById(R.id.situpprob);
-        burpeeprob = (TextView)findViewById(R.id.burpeeprob);
-        squatprob = (TextView)findViewById(R.id.squatprob);
-        setbreakprob = (TextView)findViewById(R.id.setbreakprob);
+        breakprob = (TextView) findViewById(R.id.breakprob);
+        situpprob = (TextView) findViewById(R.id.situpprob);
+        burpeeprob = (TextView) findViewById(R.id.burpeeprob);
+        squatprob = (TextView) findViewById(R.id.squatprob);
+        setbreakprob = (TextView) findViewById(R.id.setbreakprob);
         // Create Sensor Manager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         // AccelerometerSensor
@@ -55,11 +60,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         //Gyroscope Sensor
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mGeomagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         // Register sensor Listener
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mOrientationAngles, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, 100000, 100000);
+        mSensorManager.registerListener(this, mOrientationAngles, 100000, 100000);
+        mSensorManager.registerListener(this, mGravity, 100000, 100000);
+        mSensorManager.registerListener(this, mGyroscope, 100000, 100000);
+        mSensorManager.registerListener(this, mGeomagnetic, SensorManager.SENSOR_DELAY_NORMAL);
         input_signal = new ArrayList();
         xGrav = new ArrayList();
         yGrav = new ArrayList();
@@ -73,6 +80,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         xRot = new ArrayList();
         yRot = new ArrayList();
         zRot = new ArrayList();
+        input_collection = new ArrayList<>();
+        breakcount = 0;
+        situpcount = 0;
+        burpeecount = 0;
+        squatcount = 0;
         xGrav.clear();
         yGrav.clear();
         zGrav.clear();
@@ -85,63 +97,135 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         xRot.clear();
         yRot.clear();
         zRot.clear();
+// TEST MODEL
+
+
+
+
+
         input_signal.clear();
         activityInference = new ActivityInference(getApplicationContext());
 
+
+
+
+    }
+
+    private float[] toFloatArray(List<Float> list) {
+        int i = 0;
+        float[] array = new float[list.size()];
+
+        for (Float f : list) {
+            array[i++] = (f != null ? f : Float.NaN);
+        }
+        return array;
     }
 
     protected void onPause() {
         super.onPause();
-         mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this);
     }
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mOrientationAngles, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, 100000, 100000);
+        mSensorManager.registerListener(this, mOrientationAngles, 100000, 100000);
+        mSensorManager.registerListener(this, mGravity, 100000, 100000);
+        mSensorManager.registerListener(this, mGyroscope, 100000, 100000);
+        mSensorManager.registerListener(this, mGeomagnetic, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         //if (j < 40) {
+        /*xGrav.add((float)0.27);
+        yGrav.add((float)-0.96);
+        zGrav.add((float)0.06);
+        xAcc.add((float)0.04);
+        yAcc.add((float)0.03);
+        zAcc.add((float)-0.06);
+        pitch.add((float)1.29);
+        roll.add((float)1.79);
+        yaw.add((float)2.26);
+        xRot.add((float)0.16);
+        yRot.add((float)-0.76);
+        zRot.add((float)-0.29);
+*/
         activityPrediction();
         if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
+           /* xG=event.values[0]/gConst;
+            yG=event.values[1]/gConst;
+            zG=event.values[2]/gConst;
+            float xG1=(float)xG;
+            float yG1=(float)yG;
+            float zG1=(float)zG;*/
             xGrav.add(event.values[0]);
             yGrav.add(event.values[1]);
             zGrav.add(event.values[2]);
+            mGrav = event.values;
+
         }
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            /*xA=event.values[0]/gConst;
+            yA=event.values[1]/gConst;
+            zA=event.values[2]/gConst;
+            float xA1=(float)xA;
+            float yA1=(float)yA;
+            float zA1=(float)zA;*/
             xAcc.add(event.values[0]);
             yAcc.add(event.values[1]);
             zAcc.add(event.values[2]);
+
         }
-        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION){
+            /* p = event.values[1]*0.0174533;
+             r = event.values[2]*0.0174533;
+             y = event.values[0]*0.0174533;
+             float p1 = (float)p;
+             float r1 = (float)r;
+             float y1 = (float)y;*/
+
+
             pitch.add(event.values[1]);
             roll.add(event.values[2]);
             yaw.add(event.values[0]);
+
         }
+
+/*
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mGeo = event.values;
+        }
+        if (mGrav != null && mGeo != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGrav, mGeo);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);   //azimut,pitch,roll
+                pitch.add(orientation[1]);
+                roll.add(orientation[2]);
+                yaw.add(orientation[0]);
+            }
+        }
+*/
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            /* xR=event.values[0]/gConst;
+             yR=event.values[1]/gConst;
+             zR=event.values[2]/gConst;
+             float xR1=(float)xR;
+            float yR1=(float)yR;
+            float zR1=(float)zR;*/
             xRot.add(event.values[0]);
             yRot.add(event.values[1]);
             zRot.add(event.values[2]);
+
         }
-        //} else {
-
-        //for(i=0;i<40;i++)
-        // input[i] = 0;
-
-
-        // data = activityInference.getActivityProb(input);
-        //System.out.println(data);
-    }
-
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+
 
 
     private void activityPrediction() {
@@ -165,11 +249,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
             // Perform inference using Tensorflow
             float[] results = activityInference.getActivityProb(toFloatArray(input_signal));
-            breakprob.setText(Float.toString(round(results[0],2)));
-            situpprob.setText(Float.toString(round(results[1],2)));
-            burpeeprob.setText(Float.toString(round(results[2],2)));
-            squatprob.setText(Float.toString(round(results[3],2)));
-            setbreakprob.setText(Float.toString(round(results[4],2)));
+            System.out.println("result");
+            System.out.println(results[0]);
+            System.out.println(results[1]);
+            System.out.println(results[2]);
+            System.out.println(results[3]);
+            System.out.println(results[4]);
+
+            breakprob.setText(Float.toString(round(results[0], 2)));
+            situpprob.setText(Float.toString(round(results[1], 2)));
+            burpeeprob.setText(Float.toString(round(results[2], 2)));
+            squatprob.setText(Float.toString(round(results[3], 2)));
+            setbreakprob.setText(Float.toString(round(results[4], 2)));
+            int max = 0;
+            for (int i = 0; i < 5; i++) {
+                if (results[i] > results[max]) {
+                    max = i;
+                }
+
+            }
+            if (max == 0) {
+                breakcount++;
+                breakprob.setText(Integer.toString(breakcount));
+            }
+            if (max == 2) {
+                situpcount++;
+                situpprob.setText(Integer.toString(situpcount));
+            }
+            if (max == 1) {
+                burpeecount++;
+                burpeeprob.setText(Integer.toString(burpeecount));
+            }
+            if (max == 3) {
+                squatcount++;
+                squatprob.setText(Integer.toString(squatcount));
+            }
             // Clear all the values
             xGrav.clear();
             yGrav.clear();
@@ -185,6 +299,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             zRot.clear();
             input_signal.clear();
         }
+    }/*
     }
 
     private float[] toFloatArray(List<Float> list) {
@@ -209,10 +324,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             z.set(i,((z.get(i) - z_m)/z_s));
         }
     }
-/*/
+*/
     public static float round(float d, int decimalPlace) {
         BigDecimal bd = new BigDecimal(Float.toString(d));
         bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
         return bd.floatValue();
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
